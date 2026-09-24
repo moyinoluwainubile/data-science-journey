751ccb2b4b9dbf9f44c72226ed64e8a5607daf25
Moyinoluwa Inubile <moyinoluwainubile2001@gmail.com>
2026-09-24 10:48:02 +0100

    Second commit

---
+++ Week-02/hello-world_Day10_guided_task.py
@@ -0,0 +1,12 @@
+print("Hello World!") #Greeting
+print("This is my first real line of Python.") #This is my first comment
+print("This is my second.") #This line prints my second message
+#This is my bio
+print("Bio: My name is Moyinoluwa Inubile, I am currently an employee of Otaku.Hugo, as a Customer Experience agent")
+#My goal for the data science programme
+print("Goal for Data Science: My goal for this Data Science Programme is to become a seasoned professional, an industry expert, whose opinions are worth their salt in the grand scheme of things, when it comes to the Data Science/ML community")
+#Here is a fact about myself
+print("One fact about me: I am someone who loves to research about things, especially political - I try to find answers to complex societal cum political problems through my research, even though I am not really in a place of strength to help as of now - but I hope to be someday, and I truly hope that when that day does come, I will be fully equipped to make positive impact on my society")
+#Something I am looking forward to
+print("Something I am Looking Forward To: I am really looking forward to what the future will bring. I have started and stopped the Data Science journey several times, but this time around, I have restarted it with a renewed will-power, and eargarly waiting to see where I can get to, what I can achieve if I refuse to stop the journey")
+print("She said 'hello'")
